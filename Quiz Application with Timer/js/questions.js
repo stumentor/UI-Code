ddab5e895f6378345db905e6d31e7e2c4ff58006
tejasvising <85549686+tejasvising@users.noreply.github.com>
2021-12-28 11:47:27 +0530

    Add files via upload

---
+++ Quiz Application with Timer/js/questions.js
@@ -0,0 +1,50 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = {
+	"III": [{
+			"numb": 1,
+			"question": "What does HTML stand for?",
+			"answer": "Hyper Text Markup Language",
+			"options": [
+				"Hyper Text Preprocessor",
+				"Hyper Text Markup Language",
+				"Hyper Text Multiple Language",
+				"Hyper Tool Multi Language"
+			]
+		},
+		{
+			"numb": 2,
+			"question": "What does CSS stand for?",
+			"answer": "Cascading Style Sheet",
+			"options": [
+				"Common Style Sheet",
+				"Colorful Style Sheet",
+				"Computer Style Sheet",
+				"Cascading Style Sheet"
+			]
+		}
+	],
+
+	"IV": [{
+			"numb": 1,
+			"question": "What does CSS stand for?",
+			"answer": "Cascading Style Sheet",
+			"options": [
+				"Common Style Sheet",
+				"Colorful Style Sheet",
+				"Computer Style Sheet",
+				"Cascading Style Sheet"
+			]
+		},
+		{
+			"numb": 2,
+			"question": "What does CSS stand for?",
+			"answer": "Cascading Style Sheet",
+			"options": [
+				"Common Style Sheet",
+				"Colorful Style Sheet",
+				"Computer Style Sheet",
+				"Cascading Style Sheet"
+			]
+		}
+	]
+};
